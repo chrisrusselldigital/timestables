13700839129f44fde20df32053b4b72a2cf58522
Chris Russell <chris.john.russell@gmail.com>
2021-02-02 19:25:56 +0000

    Adds some design

--- tables.js
+++ tables.js
@@ -8,11 +8,13 @@ document.getElementById("secondpart").innerHTML = multiplicand;
 
 function parseAnswer() {
   if (document.getElementById('MyAnswer').value == solution) {
-    document.getElementById('feedback').innerHTML = "Correct";
+    document.getElementById('feedback').innerHTML = "Correct! The answer is <strong>" + solution + "</strong>.";
+    document.getElementById("feedback").style.color = "green";
     console.log("correct");
   }
   else {
-    document.getElementById('feedback').innerHTML = "Incorrect";
+    document.getElementById('feedback').innerHTML = "Sorry, the answer is <strong>" + solution + "</strong>.";
+    document.getElementById("feedback").style.color = "red";
     console.log("incorrect");
   }
 }
